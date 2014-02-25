6836ae283999806967629e4b9b44eed0adca0111
Peter Bex <peter@codeyellow.nl>
2014-02-25 15:03:18 +0100

    Add several other packages to the list

--- freebsd.sh
+++ freebsd.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+# Notes:
+# qt4 depends on postgresql90 and mysql55, it causes trouble if we try to use newer ones
+# Allegro-devel uses openal-soft, which conflicts with openal
+# There is an imlib2 package, but it cannot be installed (no binary?)
 su root -c "pkg install \
-	graphviz subversion \
-	libgit2 tokyocabinet \
-	postgresql93-client postgresql93-server \
-	freetds-devel mysql56-client mysql56-server"
+	graphviz subversion libsvm leptonica R gsl plotutils \
+	ploticus pkgconf libexif cairo epeg expat fcgi-devkit \
+	sdl sdl_gfx sdl_net sdl_ttf sdl_image sdl_mixer qt4 qt4-opengl \
+	g2 glfw glm glpk gts openjdk fltk blas f2c fann \
+	allegro-devel openal-soft openmpi libgit2 tokyocabinet zmq \
+	python27 postgresql90-client postgresql90-server \
+	augeas ossp-uuid webkit-gtk3 hyperestraier memcached \
+	redis \
+	freetds-devel mysql55-client mysql55-server"
