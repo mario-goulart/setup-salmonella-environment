10061ac41165d4670fab0027e1ce4c9f4afb517a
Peter Bex <peter@codeyellow.nl>
2014-02-26 08:35:51 +0100

    blas and cblas are both needed to compile the blas egg

--- freebsd.sh
+++ freebsd.sh
@@ -8,7 +8,7 @@ su root -c "pkg install \
 	graphviz subversion libsvm leptonica R gsl plotutils \
 	ploticus pkgconf libexif cairo epeg expat fcgi-devkit \
 	sdl sdl_gfx sdl_net sdl_ttf sdl_image sdl_mixer qt4 qt4-opengl \
-	g2 glfw glm glpk gts openjdk fltk blas f2c fann \
+	g2 glfw glm glpk gts openjdk fltk blas cblas f2c fann \
 	allegro-devel openal-soft openmpi libgit2 tokyocabinet zmq \
 	python27 postgresql90-client postgresql90-server \
 	augeas ossp-uuid webkit-gtk3 hyperestraier memcached \
